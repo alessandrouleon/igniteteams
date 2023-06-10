a16b7c772cd1a77a1e7a83902e28f3e19b4a62d4
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2023-06-09 21:47:46 -0400

    feat: create component loading

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from "styled-components/native";
-import { ActivityIndicator } from "react-native";
 import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
 import {
@@ -7,12 +6,13 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
