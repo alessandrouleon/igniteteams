fa4c27f987e7e65fca6f0126774b3478b8b1badb
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2023-06-11 15:54:53 -0400

    feat: create component group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Hightlight } from "@components/Hightlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
-      <Hightlight title="Turma" subtitle="joge com a sua turma" />
+      <Hightlight title="Turma" subtitle="Jogue com a sua turma" />
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,5 +2,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  padding: 0 20px;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `;
