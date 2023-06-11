86017f7f2d8c34fedb273b6c57d968472e1f735d
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2023-06-11 16:43:03 -0400

    feat: create component listEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,25 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Hightlight } from "@components/Hightlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
       <Hightlight title="Turma" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não existe grupo cadastrado..." />
+        )}
+      />
     </Container>
   );
 }
