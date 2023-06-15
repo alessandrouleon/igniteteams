da49e091f6b4b74b8e1510b911d85ac8fe278678
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2023-06-14 21:40:15 -0400

    feat: create components filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,22 +3,21 @@ import { Container, Form } from "./styles";
 import { Hightlight } from "@components/Hightlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
   return (
     <Container>
-    <Header showBackButton/>
-    <Hightlight 
-    title="Nome da turma"
-    subtitle="Adicione a galera e separe o time"
-    />
-    <Form>
-    <Input 
-    placeholder="Nome da pessoa"
-    autoCorrect={false}
-    />
-    <ButtonIcon icon="addfile"  />
-    </Form>
+      <Header showBackButton />
+      <Hightlight
+        title="Nome da turma"
+        subtitle="Adicione a galera e separe o time"
+      />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="addfile" />
+      </Form>
+      <Filter title="Turma A" isActive />
     </Container>
   );
 }
