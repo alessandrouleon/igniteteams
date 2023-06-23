39aa28f6047b9fb3e9f76412cf2111d360fdaebd
alessandro.uleon1 <alessandro.uleon@grupoicts.com.br>
2023-06-22 23:36:59 -0400

    feat: finish navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
-import { NavigationContainer } from "@react-navigation/native"; 
+import { NavigationContainer } from "@react-navigation/native";
 import { AppRouter } from "./app.routes";
+import { View } from "react-native";
+import { ThemeType, useTheme } from "styled-components/native";
 
-export function Routes(){
-  return(
-    <NavigationContainer>
-      <AppRouter/>
-    </NavigationContainer>
+export function Routes() {
+  const theme = useTheme() as ThemeType;
+  return (
+    <View style={{flex: 1, backgroundColor: theme.COLORS.GRAY_600}}>
+      <NavigationContainer>
+        <AppRouter />
+      </NavigationContainer>
+    </View>
   );
 }
